7fd37f79155bc6f39e7e15e9e8b5b8dc0373b311
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-06-29 14:32:17 +0500

    generated proto

--- script/gen-proto.sh
+++ script/gen-proto.sh
@@ -1,16 +1,6 @@
 #!/bin/zsh
 CURRENT_DIR=$1
-
-# Protobuf fayllarni saqlash uchun `genproto` katalogini o'chirib qayta yaratish
-rm -rf ${CURRENT_DIR}/genproto
-mkdir -p ${CURRENT_DIR}/genproto
-
-# Protobuf fayllarni generatsiya qilish
 for x in $(find ${CURRENT_DIR}/proto -type d); do
-  echo "Processing directory: $x"
-  protoc -I=${x} -I=${CURRENT_DIR}/proto -I /usr/local/include \
-    --go_out=${CURRENT_DIR}/genproto --go-grpc_out=${CURRENT_DIR}/genproto \
-    ${x}/*.proto
+  protoc -I=${x} -I=${CURRENT_DIR}/proto -I /usr/local/go --go_out=${CURRENT_DIR} \
+   --go-grpc_out=${CURRENT_DIR} ${x}/*.proto
 done
-
-echo "Protobuf generation completed."
