e195cfc8524c9793c0b16c88148cb93be2f69448
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-06-29 15:26:53 +0500

    birinchi method yozildi, full

--- service/userService.go
+++ service/userService.go
@@ -1,6 +1,24 @@
 package service
 
+import (
+	"context"
+	pb "userService/generated"
+	"userService/storage/postgres"
+)
 
-type Server struct{
-	
+type Server struct {
+	pb.UnimplementedUserServiceServer
+	U postgres.NewUser
+}
+
+func NewUserServer(u postgres.NewUser) *Server {
+	return &Server{U: u}
+}
+
+func (S *Server) GetUser(ctx context.Context, userId *pb.UserId) (*pb.User, error) {
+	user, err := S.U.GetUser(userId)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
