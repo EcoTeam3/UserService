d5002c0b2c423fad66cf46f9453f2bcb4cb6e569
fahriddin <fahriddinrahimberdiyev@gmail.com>
2024-06-29 16:35:06 +0500

    Service Qismi bajarildi

--- storage/postgres/user.go
+++ storage/postgres/user.go
@@ -2,6 +2,7 @@ package postgres
 
 import (
 	"database/sql"
+	"time"
 	pb "userService/generated"
 )
 
@@ -22,7 +23,36 @@ func(U *NewUser) GetUser(userId *pb.UserId)(*pb.User, error){
 							FROM 
 								Users
 							WHERE 
-								user_id = $1`, id).
+								user_id = $1 and deleted_at is null`, id).
 							Scan(&user.UserId, &user.Username, &user.Email, &user.CreatedAt)	
 	return &user, err			
+}
+
+func (U *NewUser) DeleteUser(userId *pb.UserId)(*pb.Status, error) {
+	id := userId.UserId
+	_,err := U.Db.Exec(`UPDATE Users 
+							SET
+								deleted_at = $1
+							WHERE
+								user_id = $2`,time.Now(),id)
+	if err != nil{
+		return &pb.Status{
+			Status: false,
+		},err
+	}
+	return &pb.Status{
+		Status: true,
+	},nil
+}
+
+func (U *NewUser) GetUserProfile(userId *pb.UserId)(*pb.UserProfile,error){
+	id := userId.UserId
+	userProfile := pb.UserProfile{}
+	err := U.Db.QueryRow("SELECT * FROM User_Profile WHERE user_id = $1",id).Scan(
+		&userProfile.UserId,&userProfile.FullName,&userProfile.Bio,&userProfile.Location,&userProfile.AvatarUrl,
+	)
+	if err != nil{
+		return nil,err
+	}
+	return &userProfile,nil
 }
