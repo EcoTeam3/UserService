f9771df13620cca97b51067944e1437c238faf08
Hamidjon Nuriddinov <nuriddinovhamidjon2@gmail.com>
2024-06-30 15:28:21 +0500

    1-Testdan otdi

--- storage/migrations/000002_insert_table.up.sql
+++ storage/migrations/000002_insert_table.up.sql
@@ -10,8 +10,8 @@ VALUES
 -- Insert statements for user_profiles table
 INSERT INTO user_profiles (user_id, full_name, bio, location, avatar_url)
 VALUES
-    ((SELECT id FROM users WHERE username = 'john_doe'), 'John Doe', 'Software developer and tech enthusiast.', 'San Francisco, CA', 'https://example.com/avatar/john_doe.jpg'),
-    ((SELECT id FROM users WHERE username = 'jane_smith'), 'Jane Smith', 'Digital marketer with a love for analytics.', 'New York, NY', 'https://example.com/avatar/jane_smith.jpg'),
-    ((SELECT id FROM users WHERE username = 'alice_jones'), 'Alice Jones', 'Graphic designer and illustrator.', 'Los Angeles, CA', 'https://example.com/avatar/alice_jones.jpg'),
-    ((SELECT id FROM users WHERE username = 'bob_brown'), 'Bob Brown', 'Content writer and SEO expert.', 'Chicago, IL', 'https://example.com/avatar/bob_brown.jpg'),
-    ((SELECT id FROM users WHERE username = 'charlie_davis'), 'Charlie Davis', 'Project manager with a knack for efficiency.', 'Austin, TX', 'https://example.com/avatar/charlie_davis.jpg');
+    ((SELECT user_id FROM users WHERE username = 'john_doe'), 'John Doe', 'Software developer and tech enthusiast.', 'San Francisco, CA', 'https://example.com/avatar/john_doe.jpg'),
+    ((SELECT user_id FROM users WHERE username = 'jane_smith'), 'Jane Smith', 'Digital marketer with a love for analytics.', 'New York, NY', 'https://example.com/avatar/jane_smith.jpg'),
+    ((SELECT user_id FROM users WHERE username = 'alice_jones'), 'Alice Jones', 'Graphic designer and illustrator.', 'Los Angeles, CA', 'https://example.com/avatar/alice_jones.jpg'),
+    ((SELECT user_id FROM users WHERE username = 'bob_brown'), 'Bob Brown', 'Content writer and SEO expert.', 'Chicago, IL', 'https://example.com/avatar/bob_brown.jpg'),
+    ((SELECT user_id FROM users WHERE username = 'charlie_davis'), 'Charlie Davis', 'Project manager with a knack for efficiency.', 'Austin, TX', 'https://example.com/avatar/charlie_davis.jpg');
